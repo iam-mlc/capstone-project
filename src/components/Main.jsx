42de4c73af3f7c896da0ec87ef61df5383d3ba89
iam-mlc <124239244+iam-mlc@users.noreply.github.com>
2023-02-24 20:00:53 +0200

    feat: add content of second section of  the main content

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,73 @@
 import foodImage from "../assets/restauranfood.jpg"
+import { ReactComponent as Delivery } from '../assets/delivery.svg';
+
+
+const Products = ({data}) => {
+    return (
+        <ul>
+            <List data = {data}/>
+        </ul>
+    );
+}
+const List = ({data}) =>{
+
+    const listItem = (list) => {
+
+        return list.map(item =>{
+
+            const {image, title, price, description, url} = item
+
+            return(
+                <li key={title}>
+                    <img src={image} alt={title} />
+                    <h3>{title}</h3>
+                    <h4>{price}</h4>
+                    <p>{description}</p>
+                    <div>
+                        <a href={url}>Order a delivery</a>
+                        <Delivery/>
+                    </div>
+                </li>
+            )
+        })
+    }
+
+    return(
+        <>
+            {
+
+                listItem(data)
+
+            }
+
+        </>
+    )
+}
 
 const Main = () => {
+    const products = [
+        {
+            image: require("../assets/greek-salad.jpg"),
+            title: "Greek salad",
+            price: "$12.99",
+            description: "The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. ",
+            url: ""
+        },
+        {
+            image: require("../assets/bruchetta.jpeg"),
+            title: "Bruchetta",
+            price: "$ 5.99",
+            description: "Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil. ",
+            url: ""
+        },
+        {
+            image: require("../assets/lemon-dessert.jpg"),
+            title: "Lemon Dessert",
+            price: "$ 5.00",
+            description: "This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.",
+            url: ""
+        }
+    ]
     return (
         <>
             <main className="mobile-grid medium-grid large-grid">
@@ -9,7 +76,18 @@ const Main = () => {
                     <h2>Chicago</h2>
                     <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist </p>
                     <a href="/reserve-a-table">Reserve a Table</a>
-                    <img src={foodImage} alt="restaurant food image" />
+                    <img src={foodImage} alt="restaurant food" />
+                </section>
+                <section>
+                    <div>
+                        <h2>
+                            This weeks specials!
+                        </h2>
+                        <a href="/">Online Menu</a>
+                    </div>
+                    <div>
+                        <Products data={products}/>
+                    </div>
                 </section>
             </main>
         </>
