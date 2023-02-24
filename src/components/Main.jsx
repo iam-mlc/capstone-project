15445ac892871e1895ffd562497951c3b9238bbb
iam-mlc <124239244+iam-mlc@users.noreply.github.com>
2023-02-24 18:29:41 +0200

    feat:  add first section of main content

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,16 @@
+import foodImage from "../assets/restauranfood.jpg"
+
 const Main = () => {
     return (
         <>
             <main className="mobile-grid medium-grid large-grid">
-                
+                <section>
+                    <h1>Little Lemon</h1>
+                    <h2>Chicago</h2>
+                    <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist </p>
+                    <a href="/reserve-a-table">Reserve a Table</a>
+                    <img src={foodImage} alt="restaurant food image" />
+                </section>
             </main>
         </>
     );
