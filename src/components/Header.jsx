8f9c5b194abdd98f07bc6a6af384909639cfc653
iam-mlc <124239244+iam-mlc@users.noreply.github.com>
2023-02-24 13:57:55 +0200

    add assets directory
    add assets directory and the necessary assets.
    
    add image in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ const Header = () => {
     return (
         <>
             <header className="mobile-grid medium-grid large-grid">
+                <img src="../assets/Logo.svg" alt="logo"/>
                 <Nav/>
             </header>
         </>
