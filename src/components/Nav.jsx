263c338fdf92ab55f011a3e9879500373efa98a9
iam-mlc <124239244+iam-mlc@users.noreply.github.com>
2023-02-24 11:26:54 +0200

    add links
    add links and  a function that converts a name to a url

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,3 +1,37 @@
+
+const links = [
+    {
+        name: "Home",
+        url: nameToURL(this.name)
+    },
+    {
+        name: "About",
+        url: nameToURL(this.name)
+    },
+    {
+        name: "Menu",
+        url: nameToURL(this.name)
+    },
+    {
+        name: "Reservations",
+        url: nameToURL(this.name)
+    },
+    {
+        name: "Order online",
+        url: nameToURL(this.name)
+    },
+    {
+        name: "Login",
+        url: nameToURL(this.name)
+    }
+];
+const nameToURL = (name) =>{
+    const lowercase = name.toLowerCase();
+    const url = lowercase.replace(/\s+/g, "-")
+
+    return url
+}
+
 const Nav = () => {
     return (
         <>
