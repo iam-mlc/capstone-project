92f1d3290c39f66ae962ea04700c8f893583a60b
iam-mlc <124239244+iam-mlc@users.noreply.github.com>
2023-03-01 11:47:05 +0200

    feat(App): add browserRouter and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css';
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header';
-import Main from './components/Main/Main'
+import Home from './pages/Home/Home'
+import { BrowserRouter, Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <>
+    <BrowserRouter>
       <Header/>
-      <Main/>
+      <Routes>
+        <Route index element={<Home/>}/>
+      </Routes>
       <Footer/>
+    </BrowserRouter>
     </>
   );
 }
