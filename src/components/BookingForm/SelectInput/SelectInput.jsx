a822be6638074d8f260f04d2c49b21d5438762f9
iam-mlc <124239244+iam-mlc@users.noreply.github.com>
2023-03-03 16:57:09 +0200

    feat(SelectInput): add new component
    This component is used to render the select field and it's corresponding options

---
+++ src/components/BookingForm/SelectInput/SelectInput.jsx
@@ -0,0 +1,35 @@
+import toCamelCase from "../../../utils/toCamelCase";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+
+const SelectInput = ({ label, formik, data }) => {
+
+    const inputName = toCamelCase(label)
+
+  return (
+    <>
+      <label htmlFor={inputName}>{label}</label>
+      <select name={inputName} id={inputName} {...formik.getFieldProps(inputName)}>
+        <OptionsInput data = {data}/>
+      </select>
+      <ErrorMessage name={inputName} formik={formik} />
+    </>
+  );
+};
+
+const OptionsInput = ({data}) => {
+
+    const options = data.map((option) =>{
+        return(
+            <option value={`${option.value}`}>{option.value}</option>
+        )
+    })
+
+    return (
+        <>
+            { options }
+        </>
+     );
+}
+
+
+export default SelectInput;
