eb75e42c6c49252b623c467fd5cabdbbb18e8346
iam-mlc <124239244+iam-mlc@users.noreply.github.com>
2023-03-03 20:58:42 +0200

    refactor(BookingForm): Add DRY code

--- src/components/BookingForm/BookingForm.jsx
+++ src/components/BookingForm/BookingForm.jsx
@@ -1,83 +1,76 @@
 import Button from "../Button/Button";
 import styles from "./BookingForm.module.css";
-import { useFormik } from 'formik';
-import { object, string, number, date} from 'yup';
+import { useFormik } from "formik";
 import { useState } from "react";
 import TextInput from "./TextInput/TextInput";
-import ErrorMessage from "./ErrorMessage/ErrorMessage";
 import DateInput from "./DateInput/DateInput";
 import SelectInput from "./SelectInput/SelectInput";
-import bookingHours from "../../utils/bookingHours";
-import bookingOccasion from "../../utils/bookingOccasion";
+import bookingHours from "../../utils/bookingForm/bookingHours";
+import bookingOccasion from "../../utils/bookingForm/bookingOccasion";
 import NumberInput from "./NumberInput/NumberInput";
-
+import validationData from "../../utils/bookingForm/validationData";
+import initialValues from "../../utils/bookingForm/formik/initialValues";
+import bookingSchema from "../../utils/bookingForm/formik/validationSchema";
 
 const BookingForm = () => {
-    const today = new Date();
-    const day = today.getDate();
-    const year = today.getFullYear();
-    const month = today.getMonth();
-    const minimumDate = `${year}-${month + 1 }-${day}`
 
-    const [isValid, setValid] = useState(true)
+    const [isValid, setValid] = useState(true);
 
-    const bookingSchema = object({
-        firstName: string().min(2, "This first name is too short").max(50, "This first name is too long").required("Please write your first name"),
-        lastName: string().min(2, "This last name is too short").max(50, "This last name is too long").required("Please write your last name"),
-        chooseDate: date().required("Please choose a date").min(minimumDate, "Please choose a date starting from today"),
-        numberOfGuests: number().integer().min(1,"Number has to be larger than 0").max(10, `Sorry ! Our tables only take a maximum of 10 guests`).required("Please write the number of guests"),
-    });
+  const [
+    firstName,
+    lastName,
+    chooseDate,
+    chooseTime,
+    numberOfGuests,
+    occasion,
+  ] = validationData;
 
-    const formik = useFormik({
-        initialValues: {
-            firstName: "",
-            lastName: "",
-            chooseDate:"",
-            chooseTime: "17:00",
-            numberOfGuests: "",
-            occasion: "Birthday",
-        },
-        validationSchema: bookingSchema,
-        onSubmit: (values, {setSubmitting, resetForm}) =>{
-            window.alert(JSON.stringify(values, null, 2))
-            setSubmitting(false)
-            resetForm()
-        }
-    });
+  const formik = useFormik({
+    initialValues: initialValues,
+    validationSchema: bookingSchema,
+    onSubmit: (values, { setSubmitting, resetForm }) => {
+      window.alert(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+      resetForm();
+    },
+  });
 
-    const handleSubmit = (e) =>{
-        e.preventDefault()
-        formik.handleSubmit()
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    formik.handleSubmit();
+  };
 
-
-    return (
-        <>
-            <form onSubmit={handleSubmit} className={`${styles.form}`}>
-                <div className={`${styles.single_col}`}>
-                    <TextInput text ={"First Name"} formik = {formik}/>
-                </div>
-                <div className={`${styles.single_col}`}>
-                    <TextInput text ={"Last Name"} formik = {formik}/>
-                </div>
-                <div className={`${styles.single_col}`}>
-                    <DateInput label={"Choose date"} formik={formik}/>
-                </div>
-                <div className={`${styles.single_col}`}>
-                    <SelectInput label={"Choose time"} formik={formik} data={bookingHours}/>
-                </div>
-                <div className={`${styles.single_col}`}>
-                    <NumberInput label={"Number of guests"} formik={formik}/>
-                </div>
-                <div className={`${styles.single_col}`}>
-                    <SelectInput label={"Occasion"} formik ={formik} data={bookingOccasion}/>
-                </div >
-                <div className={`${styles.single_col} ${styles.button}`}>
-                    <Button text={"Make Your reservation"} role="submit"/>
-                </div>
-            </form>
-        </>
-     );
-}
+  return (
+    <>
+      <form onSubmit={handleSubmit} className={`${styles.form}`}>
+        <div className={`${styles.single_col}`}>
+          <TextInput text={firstName.label} formik={formik} />
+        </div>
+        <div className={`${styles.single_col}`}>
+          <TextInput text={lastName.label} formik={formik} />
+        </div>
+        <div className={`${styles.single_col}`}>
+          <DateInput label={chooseDate.label} formik={formik} />
+        </div>
+        <div className={`${styles.single_col}`}>
+          <SelectInput label={chooseTime.label} formik={formik} data={bookingHours} />
+        </div>
+        <div className={`${styles.single_col}`}>
+          <NumberInput label={numberOfGuests.label} formik={formik} />
+        </div>
+        <div className={`${styles.single_col}`}>
+          <SelectInput
+            label={occasion.label}
+            formik={formik}
+            data={bookingOccasion}
+          />
+        </div>
+        <div className={`${styles.single_col} ${styles.button}`}>
+          <Button text={"Make Your reservation"} role="submit" />
+        </div>
+      </form>
+    </>
+  );
+};
 
 export default BookingForm;
