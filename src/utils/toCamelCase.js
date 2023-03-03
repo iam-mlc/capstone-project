0fa32b868b176607e7997272a03d63936d23d678
iam-mlc <124239244+iam-mlc@users.noreply.github.com>
2023-03-03 11:14:18 +0200

    feat(toCamelCase): add new function
    The toCamelCase conversts a string with white spaces, hyphens or underlines in to a camelCase string

---
+++ src/utils/toCamelCase.js
@@ -0,0 +1,8 @@
+
+const toCamelCase = string => {
+    return string.replace(/[-_\s]+(.)?/g, (_, c) => c ? c.toUpperCase() : '').replace(/^(.)/, (m, c) => c.toLowerCase());
+};
+
+export default toCamelCase
+
+
