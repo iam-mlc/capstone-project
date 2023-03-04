604ac6217d3b89a11c5aae318c665417517c7b89
iam-mlc <124239244+iam-mlc@users.noreply.github.com>
2023-03-04 13:20:57 +0200

    feat(FirstSection): add new component

---
+++ src/pages/Booking/Sections/FirstSection/FirstSection.jsx
@@ -0,0 +1,25 @@
+import styles from "./FirstSection.module.css"
+import restaurantImage from "../../../../assets/restaurant.jpg"
+
+const FirstSection = () => {
+    return (
+        <div className={styles.hero}>
+            <div className={styles.details}>
+              <div className={styles.titles}>
+                <h1>Book a table</h1>
+                <h2>Reservations are open</h2>
+              </div>
+              <div className={styles.description}>
+                <p>
+                    Please fill the form bellow so you can guarantee a table for your friends and family. We really want you to enjoy the delicious meals we prepared today !
+                </p>
+              </div>
+            </div>
+            <div className={styles.image}>
+              <img src={restaurantImage} alt="" />
+            </div>
+          </div>
+     );
+}
+
+export default FirstSection;
