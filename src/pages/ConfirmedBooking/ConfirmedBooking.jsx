63e19021c9a232362d3d29bb07cfaaf4f8ca4e45
iam-mlc <124239244+iam-mlc@users.noreply.github.com>
2023-03-05 18:56:57 +0200

    feat(ConfirmedBooking): Create page
    This page is displayed after the booking form is submitted successfully

---
+++ src/pages/ConfirmedBooking/ConfirmedBooking.jsx
@@ -0,0 +1,11 @@
+const ConfirmedBooking = () => {
+    return ( 
+        <main>
+            <section>
+                <p>We look forward to receive you. Your table is reserverd for guests at </p>
+            </section>
+        </main>
+     );
+}
+ 
+export default ConfirmedBooking;
