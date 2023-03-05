941e0516a1c250272013345479db6b1ac98a2ac4
iam-mlc <124239244+iam-mlc@users.noreply.github.com>
2023-03-05 11:31:08 +0200

    feat(Booking): Pass callbacks as props
    Pass functions as props to receive data from the BookingForm component

--- src/pages/Booking/Booking.jsx
+++ src/pages/Booking/Booking.jsx
@@ -1,26 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BookingForm from "./Sections/SecondSection/BookingForm";
 import grids from "../../utils/cssGridClassNames";
 import styles from "./Booking.module.css";
 import bookingHours from "../../utils/bookingForm/bookingHours";
 import FirstSection from "./Sections/FirstSection/FirstSection";
+import { fetchAPI, submitAPI } from "../../utils/bookingForm/api";
 
 const Booking = () => {
   const [availableTimes, setAvailableTimes] = useState([...bookingHours]);
-  const updateTimes = (slots) =>{
-      setAvailableTimes(slots)
-  }
-  const initializeTimes = () =>{
+  const [data, setData] = useState([]);
 
-  }
+
+  const updateTimes = (date) => {
+      const times = date === "" ? fetchAPI(new Date) : fetchAPI(new Date(date))
+      setAvailableTimes(times)
+  };
+  const handleData = (data) => {
+      setData(data)
+  };
+
 
   return (
     <main className={`${grids} ${styles.main}`}>
       <section>
-        <FirstSection/>
+        <FirstSection />
       </section>
       <section>
-        <BookingForm availableTimes={availableTimes}/>
+        <BookingForm availableTimes={availableTimes} handleData={handleData} updateTimes={updateTimes} />
       </section>
     </main>
   );
